69c2e7dda4e4879d5df4f30a598b46787144adce
Damien Le Thiec <damien.lethiec@gmail.com>
2018-03-06 15:36:37 +0700

    End of the template generation

---
+++ config/initializers/generators.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+Rails.application.config.generators do |g|
+  # Disable generators we don't need.
+  g.javascripts false
+  g.stylesheets false
+  g.assets false
+  g.helper false
+  g.test_framework false
+  g.channel assets: false
+end
